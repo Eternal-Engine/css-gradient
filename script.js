ea35a0a7bcf6b8f629bc3495b8399c5214361ef7
Ninovation <65780729+Ninovation-lab@users.noreply.github.com>
2020-05-25 07:43:59 +0200

    Add copy paste information

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let copyPaste = document.querySelector("h5");
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
@@ -5,6 +6,7 @@ let body = document.getElementById("gradient");
 
 let gradientSetter = () => {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    copyPaste.textContent = "VOILA!! Here's Your CSS Code:";
     css.textContent = body.style.background + ";";
 }
 
